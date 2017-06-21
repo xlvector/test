a319d41a32460fa3c55b13fd7032bfd5080479a8
Xiang Liang <xiangliang@bytedance.com>
2017-06-21 14:07:40 +0800

    link pthread and gfortran

--- gemm/build.sh
+++ gemm/build.sh
@@ -1,3 +1,3 @@
 g++ --std=c++11 -o run main.cpp -I/data01/home/xiangliang/3rd/include/eigen3 -I/opt/intel/mkl/include -L/opt/intel/mkl/lib/intel64/ -lmkl_rt -O3 -march=native
 
-g++ --std=c++11 -o openblas openblas.cpp -I/data01/home/xiangliang/3rd/include/ -O3 -lopenblas
+g++ --std=c++11 -o openblas openblas.cpp -I/data01/home/xiangliang/3rd/include/ -L/data01/home/xiangliang/3rd/lib/ -O3 -lopenblas -lpthread -lgfortran
